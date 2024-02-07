6c9021fc315f0743fcb0f1aa9293adde9322bf50
Farhad Maten <farhad.maten@gmail.com>
2024-02-07 08:17:43 -0500

    finalized

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   // images: {
   //   unoptimized: true
   // },
-  // basePath: "/my-robofriends",
+  basePath: "/my-robofriends",
   // images: {
   //   remotePatterns: [
   //     {
