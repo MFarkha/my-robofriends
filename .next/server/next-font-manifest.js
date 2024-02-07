57cec0e217e687aee251748c7def331153c5df05
Farhad Maten <farhad.maten@gmail.com>
2024-02-06 22:12:22 -0500

    modified actions

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,2 @@
+self.__NEXT_FONT_MANIFEST =
+  '{"pages":{},"app":{"/Users/famaten/dev/web/my-robofriends/app/layout":["static/media/f1f65b6c9322702f-s.p.woff"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,2 @@
+self.__RSC_SERVER_MANIFEST =
+  '{\n  "node": {},\n  "edge": {},\n  "encryptionKey": "RNGveWq2GccdMVgtm8i/U72UolsgLqrK95n+v7/d2KU="\n}';
